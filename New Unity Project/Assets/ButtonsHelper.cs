8bc1aa0828e6fdcfde0c463577c38904ae2a063d
Happaro <happaro@gmail.com>
2017-11-04 23:36:09 +0200

    init buttons

---
+++ New Unity Project/Assets/ButtonsHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsHelper : MonoBehaviour
+{
+	public PlayerController player;
+
+	public void PunchArm()
+	{
+		player.PunchHand();
+	}
+
+	public void PunchLeg()
+	{
+		player.PunchLeg();
+	}
+
+	public void Jump()
+	{
+		player.Jump();
+	}
+
+	public void Move(int direction)
+	{
+		player.Move(direction);
+	}
+
+	public void Stop(int direction)
+	{
+		player.Stop(direction);
+	}
+}
---
+++ New Unity Project/Assets/MyNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class MyNetworkManager : NetworkManager
+{
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		base.OnServerAddPlayer(conn, playerControllerId);
+		FindObjectOfType<ButtonsHelper>().player = FindObjectOfType<PlayerController>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.O))
+			Debug.LogWarning(GameObject.FindObjectOfType<PlayerController>() != null);
+	}
+}
