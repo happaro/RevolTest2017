72ef2cafd0386a8b79db792ca38699e533cbe969
Happaro <happaro@gmail.com>
2017-11-04 13:12:40 +0200

    SAM SHEEEET

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -5,28 +5,66 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float speed;
+	public float jumpForce;
 	public int side = 1;
 
 	public Animator armController, legController, spineController;
 	bool isRightPunch;
 	bool isRightPunchLeg;
+	public Rigidbody2D body;
+
+	private int currentDirection;
+
 
 	void Update ()
 	{
-		if (Input.GetKeyDown(KeyCode.Space))
+
+		transform.position += Vector3.right * currentDirection * speed * Time.deltaTime * side;
+
+		legController.SetBool("isWalking", currentDirection != 0);
+		if (Input.GetKeyDown(KeyCode.F))
 		{
 			isRightPunch = !isRightPunch;
 			armController.SetTrigger(isRightPunch ? "punchRight" : "punchLeft");
 		}
 
-		if (Input.GetKeyDown(KeyCode.KeypadEnter))
+		if (Input.GetKeyDown(KeyCode.G))
 		{
 			isRightPunchLeg = !isRightPunchLeg;
 			legController.SetTrigger(isRightPunchLeg ? "punchRight" : "punchLeft");
 		}
 
-		var inputX = Input.GetAxis("Horizontal");
-		transform.position += Vector3.right * inputX * speed * Time.deltaTime * side;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			body.AddForce(Vector2.up * jumpForce);
+		}
+		//var inputX = Input.GetAxis("Horizontal");
+		//transform.position += Vector3.right * inputX * speed * Time.deltaTime * side;
 		//legController.SetBool("isWalking", inputX != 0);
+	}
+
+	public void Move(int direction)
+	{
+		currentDirection = direction;
+	}
+
+	public void Stop(int direction)
+	{
+		if (direction == currentDirection)
+		{
+			currentDirection = 0;
+		}
+	}
+
+	public void PunchHand()
+	{
+		isRightPunch = !isRightPunch;
+		armController.SetTrigger(isRightPunch ? "punchRight" : "punchLeft");
+	}
+
+	public void PunchLeg()
+	{
+		isRightPunchLeg = !isRightPunchLeg;
+		legController.SetTrigger(isRightPunchLeg ? "punchRight" : "punchLeft");
 	}
 }
---
+++ New Unity Project/Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+	public float timer;
+	public Text text;
+	public GameObject IK;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+			Time.timeScale = 0;
+		timer += Time.deltaTime;
+		//Debug.LogWarning(timer);
+		text.text = (int)timer + "\n" + IK.transform.position.ToString();
+	}
+}
