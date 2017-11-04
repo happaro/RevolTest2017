47379e28798152a0fb7bf5e62b9cdb645aa7215e
Dima Lupyak <dima.lupyak@gmail.com>
2017-11-04 22:13:00 +0200

    Added network scene

---
+++ New Unity Project/Assets/HostGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+    [SerializeField]
+    private uint roomSize = 2;
+    private string roomName = "Fight";
+    private UnityEngine.Networking.NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = UnityEngine.Networking.NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void CreateRoom()
+    {
+        networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+    }
+}
---
+++ New Unity Project/Assets/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    public Behaviour[] componentsToDisable;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+            DisableAllAnimators(transform);
+        }
+
+    }
+
+    private void DisableAllAnimators(Transform t)
+    {
+        foreach (var animator in  t.gameObject.GetComponents<Animator>())
+        {
+            animator.enabled = false;
+        }
+        foreach (object obj in t)
+        {
+            Transform child = (Transform)obj;
+            DisableAllAnimators(child);
+        }
+    }
+}
